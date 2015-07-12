396ecc4e67ddd9a237bcf4182245b0718524735a
yarray <08to09@gmail.com>
2015-07-12 14:02:48 +0200

    sample table control

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,16 @@
-var say = require('./worker');
+// var say = require('./worker');
+//
+// setTimeout(say, 2000);
+document.addEventListener("DOMContentLoaded", function(event) {
+    var TableControl = require('./table.js');
+    var tc = new TableControl(document.getElementById('table'),
+                     ['1', '2', '3'],
+                     [[123, 456, 789], [8, 80, 8080]]);
+    tc.onSelected(function(data) {
+        alert(data);
+    });
 
-setTimeout(say, 2000);
+    // setTimeout(function() {
+    //     tc.bind([[3, 5, 5], [5, 9, 3]]);
+    // }, 2000);
+});
