ed0cf0c043a038ad02a6ef52db201c792f712b86
tumluliu <nudtlliu@gmail.com>
2015-07-18 00:37:34 +0800

    use real data from trackservice API instead of fake data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,205 @@
 // var say = require('./worker');
 //
 // setTimeout(say, 2000);
+var sampleData = {
+  "num_results": 2559, 
+  "objects": [
+    {
+      "2D length": 17657.0, 
+      "3D length": 17660.0, 
+      "Downhill": 88.82, 
+      "End lat": 48.1768849, 
+      "End lon": 11.5942549, 
+      "Ended at": "None", 
+      "ID": 4, 
+      "Max speed": 0.0, 
+      "Moving time": "None", 
+      "Points": 208, 
+      "Segments": 1, 
+      "Start lat": 48.075854, 
+      "Start lon": 11.4860349, 
+      "Started at": "None", 
+      "Stopped time": "None", 
+      "Uphill": 18.82
+    }, 
+    {
+      "2D length": 1254.0, 
+      "3D length": 1255.0, 
+      "Downhill": 4.88, 
+      "End lat": 48.1048797, 
+      "End lon": 11.5357447, 
+      "Ended at": "2015-05-10 13:10:50", 
+      "ID": 5, 
+      "Max speed": 1.47, 
+      "Moving time": "0:18:28", 
+      "Points": 1224, 
+      "Segments": 1, 
+      "Start lat": 48.1070819, 
+      "Start lon": 11.5357299, 
+      "Started at": "2015-05-10 12:50:27", 
+      "Stopped time": "0:01:55", 
+      "Uphill": 11.88
+    }, 
+    {
+      "2D length": 16769.0, 
+      "3D length": 16772.0, 
+      "Downhill": 97.03, 
+      "End lat": 48.220534, 
+      "End lon": 11.5873049, 
+      "Ended at": "2015-05-10 13:11:51", 
+      "ID": 6, 
+      "Max speed": 6.59, 
+      "Moving time": "1:39:00", 
+      "Points": 3568, 
+      "Segments": 1, 
+      "Start lat": 48.1754351, 
+      "Start lon": 11.5897827, 
+      "Started at": "2015-05-10 11:25:34", 
+      "Stopped time": "0:07:17", 
+      "Uphill": 84.03
+    }, 
+    {
+      "2D length": 140535.0, 
+      "3D length": 140564.0, 
+      "Downhill": 433.07, 
+      "End lat": 48.2465911, 
+      "End lon": 11.4473443, 
+      "Ended at": "None", 
+      "ID": 7, 
+      "Max speed": 0.0, 
+      "Moving time": "None", 
+      "Points": 5000, 
+      "Segments": 1, 
+      "Start lat": 48.129556, 
+      "Start lon": 11.448957, 
+      "Started at": "None", 
+      "Stopped time": "None", 
+      "Uphill": 377.07
+    }, 
+    {
+      "2D length": 149748.0, 
+      "3D length": 149821.0, 
+      "Downhill": 470.66, 
+      "End lat": 48.1802201, 
+      "End lon": 11.4024782, 
+      "Ended at": "None", 
+      "ID": 8, 
+      "Max speed": 0.0, 
+      "Moving time": "None", 
+      "Points": 5000, 
+      "Segments": 1, 
+      "Start lat": 48.2467766, 
+      "Start lon": 11.4470844, 
+      "Started at": "None", 
+      "Stopped time": "None", 
+      "Uphill": 503.66
+    }, 
+    {
+      "2D length": 153199.0, 
+      "3D length": 153225.0, 
+      "Downhill": 407.43, 
+      "End lat": 48.18248, 
+      "End lon": 11.6069536, 
+      "Ended at": "None", 
+      "ID": 9, 
+      "Max speed": 0.0, 
+      "Moving time": "None", 
+      "Points": 5000, 
+      "Segments": 1, 
+      "Start lat": 48.1806064, 
+      "Start lon": 11.4025426, 
+      "Started at": "None", 
+      "Stopped time": "None", 
+      "Uphill": 389.43
+    }, 
+    {
+      "2D length": 243155.0, 
+      "3D length": 243182.0, 
+      "Downhill": 189.63, 
+      "End lat": 48.148076, 
+      "End lon": 11.45961, 
+      "Ended at": "None", 
+      "ID": 10, 
+      "Max speed": 0.0, 
+      "Moving time": "None", 
+      "Points": 5000, 
+      "Segments": 1, 
+      "Start lat": 48.1824352, 
+      "Start lon": 11.6069072, 
+      "Started at": "None", 
+      "Stopped time": "None", 
+      "Uphill": 218.63
+    }, 
+    {
+      "2D length": 116182.0, 
+      "3D length": 116217.0, 
+      "Downhill": 539.06, 
+      "End lat": 48.1643753, 
+      "End lon": 11.5861788, 
+      "Ended at": "None", 
+      "ID": 11, 
+      "Max speed": 0.0, 
+      "Moving time": "None", 
+      "Points": 5000, 
+      "Segments": 1, 
+      "Start lat": 48.148093, 
+      "Start lon": 11.459626, 
+      "Started at": "None", 
+      "Stopped time": "None", 
+      "Uphill": 523.06
+    }, 
+    {
+      "2D length": 238791.0, 
+      "3D length": 238845.0, 
+      "Downhill": 628.82, 
+      "End lat": 48.1274986, 
+      "End lon": 11.5950608, 
+      "Ended at": "None", 
+      "ID": 12, 
+      "Max speed": 0.0, 
+      "Moving time": "None", 
+      "Points": 5000, 
+      "Segments": 1, 
+      "Start lat": 48.1642685, 
+      "Start lon": 11.5861292, 
+      "Started at": "None", 
+      "Stopped time": "None", 
+      "Uphill": 649.82
+    }, 
+    {
+      "2D length": 35228.0, 
+      "3D length": 35242.0, 
+      "Downhill": 133.63, 
+      "End lat": 48.1183483, 
+      "End lon": 11.6857133, 
+      "Ended at": "None", 
+      "ID": 13, 
+      "Max speed": 0.0, 
+      "Moving time": "None", 
+      "Points": 5000, 
+      "Segments": 1, 
+      "Start lat": 48.1274772, 
+      "Start lon": 11.5951252, 
+      "Started at": "None", 
+      "Stopped time": "None", 
+      "Uphill": 130.63
+    }
+  ], 
+  "page": 1, 
+  "total_pages": 256
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
     var TableControl = require('./table.js');
-    var tc = new TableControl(document.getElementById('table'),
-                     ['1', '2', '3'],
-                     [[123, 456, 789], [8, 80, 8080]]);
+    var keys = Object.keys(sampleData.objects[0]);
+    var values = [];
+    sampleData.objects.forEach(function(data) {
+        var row = Object.keys(data).map(function(key){
+            return data[key];
+        });
+        values.push(row);
+    });
+    var tc = new TableControl(document.getElementById('table'), keys, values);
     tc.onSelected(function(data) {
         alert(data);
     });
